61da60664763db6445cdba82715ddbf87b74d739
Emil Haukeland <emil.haukeland@gmail.com>
2020-10-25 22:33:57 +0100

    Added main

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/emilhauk/identity-api
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	go.mongodb.org/mongo-driver v1.4.2
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"github.com/emilhauk/identity-api/config"
+	"github.com/emilhauk/identity-api/endpoint"
+	"github.com/emilhauk/identity-api/store"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"time"
+)
+
+func setupRoutes(endpoints *endpoint.Endpoints) {
+	http.HandleFunc("/login", endpoints.LoginHandler)
+}
+
+func main() {
+	c := config.NewConfig()
+
+	mongoClient, err := mongo.NewClient(options.Client().ApplyURI(c.MongoDBUrl))
+	if err != nil {
+		log.Panic(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = mongoClient.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mongoClient.Disconnect(ctx)
+	mongoStore := store.NewMongoStore(mongoClient)
+
+	endpoints := endpoint.NewEndpoints(&mongoStore, c)
+
+	setupRoutes(endpoints)
+	log.Fatal(http.ListenAndServe(c.Host, nil))
+}
